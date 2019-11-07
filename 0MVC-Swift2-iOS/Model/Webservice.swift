4121792e8564db4a89df8658467ec0fec0407696
macOS <macos@macOSs-iMac.local>
2019-11-07 10:51:47 -0800

    MVC Swift 2

---
+++ 0MVC-Swift2-iOS/Model/Webservice.swift
@@ -0,0 +1,17 @@
+//
+//  Webservice.swift
+//  0MVC-Swift2-iOS
+//
+//  Created by macOS on 07/11/19.
+//  Copyright © 2019 macOS. All rights reserved.
+//
+
+import Foundation
+
+class Webservice:NSObject {
+    
+    func someFunctionWithClosure(a:String,and b :String, handler: (Int,Int)->Void){
+        
+    }
+    //someFunctionWithClosure("a","b"){(num1,num2)->Void in }
+}
